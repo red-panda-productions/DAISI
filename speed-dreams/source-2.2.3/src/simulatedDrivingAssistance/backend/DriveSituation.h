53fb8145cc6bf9c2e1b18acb1b6fb5870bedddcc
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-02-24 10:40:59 +0100

    backend headers for front end

---
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/DriveSituation.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "EnvironmentInfo.h"
+#include "CarInfo.h"
+#include "PlayerInfo.h"
+class DriveSituation
+{
+public:
+	EnvironmentInfo GetEnvironmentInfo();
+	CarInfo GetCarInfo();
+	PlayerInfo GetPlayerInfo();
+};
