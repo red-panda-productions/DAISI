2f7f4368c063efe053b2ff3a3ae562e925cc752d
shoekzema <57985160+shoekzema@users.noreply.github.com>
2022-03-08 12:24:22 +0100

    Changed documentation

--- speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/InterventionMenu.cpp
+++ speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/InterventionMenu.cpp
@@ -4,7 +4,7 @@
 #include "InterventionMenu.h"
 
 
-/* Available intervention names and associated values {0,1,2,3,4} */
+// Available intervention names and associated values {0,1,2,3,4}
 static const char *s_interventionTypes[] = { "no intervention",
                                            "show intervention",
                                            "ask for intervention",
@@ -13,20 +13,18 @@ static const char *s_interventionTypes[] = { "no intervention",
 static const int s_nrInterventions = sizeof(s_interventionTypes) / sizeof(s_interventionTypes[0]);
 static int s_curInterventionTypeIndex = 0;
 
-/* GUI label ids */
+// GUI label ids
 static int s_interventionTypeId;
 
-/* GUI screen handles */
+// GUI screen handles
 static void	*s_scrHandle = NULL;
 static void	*s_nextHandle = NULL;
 
-/* Mediator to handle communication between front-end and back-end */
+// Mediator to handle communication between front-end and back-end
 Mediator p_mediator;
 
 
-/// <summary>
-/// Loads the intervention type from the backend config
-/// </summary>
+/// @brief Loads the intervention type from the backend config
 static void ReadInterventionType(void)
 {
     s_curInterventionTypeIndex = p_mediator.GetInterventionType();
@@ -34,9 +32,7 @@ static void ReadInterventionType(void)
     GfuiLabelSetText(s_scrHandle, s_interventionTypeId, s_interventionTypes[s_curInterventionTypeIndex]);
 }
 
-/// <summary>
-/// Saves the chosen intervention type into the backend config
-/// </summary>
+/// @brief Saves the chosen intervention type into the backend config
 static void SaveInterventionType(void * /* dummy */)
 {
     p_mediator.SetInterventionType(s_curInterventionTypeIndex);
@@ -45,10 +41,8 @@ static void SaveInterventionType(void * /* dummy */)
     GfuiScreenActivate(s_nextHandle);
 }
 
-/// <summary>
-/// Changes the interventionType selected and displayed on screen
-/// </summary>
-/// <param name="index"> The index of the selected interventionType </param>
+/// @brief Changes the interventionType selected and displayed on screen
+/// @param index The index of the selected interventionType
 static void ChangeInterventionType(void *index)
 {
     // delta is 1 if the right arrow has been pressed and -1 if the left arrow has been pressed
@@ -59,35 +53,31 @@ static void ChangeInterventionType(void *index)
     GfuiLabelSetText(s_scrHandle, s_interventionTypeId, s_interventionTypes[s_curInterventionTypeIndex]);
 }
 
-/// <summary>
-/// Sets the current interventionType displayed in the options correctly
-/// </summary>
+/// @brief Sets the current interventionType displayed in the options correctly
 static void OnActivate(void * /* dummy */)
 {
     ReadInterventionType();
 }
 
 
-/// <summary>
-/// Initializes the intervention menu
-/// </summary>
-/// <param name="nextMenu"> The scrHandle of the next menu </param>
-/// <return> Returns the interventionMenu scrHandle </return>
+/// @brief Initializes the intervention menu
+/// @param nextMenu The scrHandle of the next menu
+/// @return The interventionMenu scrHandle
 void *InterventionMenuInit(void* nextMenu)
 {
-    /* return if screen already created */
+    // return if screen already created
     if (s_scrHandle) {
         return s_scrHandle;
     }
 
-    /* otherwise, create the screen */
+    // otherwise, create the screen
     s_scrHandle = GfuiScreenCreate((float*)NULL, NULL, OnActivate, NULL, (tfuiCallback)NULL, 1);
     s_nextHandle = nextMenu;
 
     void *param = GfuiMenuLoad("interventionmenu.xml");
     GfuiMenuCreateStaticControls(s_scrHandle, param);
 
-    /* menu button controls: arrow buttons, label hover-ability and apply button */
+    // menu button controls: arrow buttons, label hover-ability and apply button
     GfuiMenuCreateButtonControl(s_scrHandle,param,"interventionleftarrow",(void*)-1,ChangeInterventionType);
     GfuiMenuCreateButtonControl(s_scrHandle,param,"interventionrightarrow",(void*)1,ChangeInterventionType);
     s_interventionTypeId = GfuiMenuCreateLabelControl(s_scrHandle,param,"interventionlabel");
@@ -95,7 +85,7 @@ void *InterventionMenuInit(void* nextMenu)
 
     GfParmReleaseHandle(param);
 
-    /* keyboard button controls */
+    // keyboard button controls
     GfuiAddKey(s_scrHandle, GFUIK_RETURN, "Apply", NULL, SaveInterventionType, NULL);
     GfuiAddKey(s_scrHandle, GFUIK_F1, "Help", s_scrHandle, GfuiHelpScreen, NULL);
     GfuiAddKey(s_scrHandle, GFUIK_F12, "Screen-Shot", NULL, GfuiScreenShot, NULL);
@@ -105,10 +95,8 @@ void *InterventionMenuInit(void* nextMenu)
     return s_scrHandle;
 }
 
-/// <summary>
-/// Activates the intervention menu screen
-/// </summary>
-/// <return> Returns 0 if successful, otherwise -1 </return>
+/// @brief Activates the intervention menu screen
+/// @return 0 if successful, otherwise -1
 int InterventionMenuRun(void)
 {
     GfuiScreenActivate(s_scrHandle);
