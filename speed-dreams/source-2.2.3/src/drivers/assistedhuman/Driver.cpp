7ba2e877f7fa04f0e7efa3d8d99c986feedfe8c2
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2022-03-08 10:25:58 +0100

    Fix build failing due to missing library references

--- speed-dreams/source-2.2.3/src/drivers/assistedhuman/Driver.cpp
+++ speed-dreams/source-2.2.3/src/drivers/assistedhuman/Driver.cpp
@@ -7,7 +7,7 @@ Driver::Driver(int p_index, const char* p_name) : m_index(p_index), m_humanDrive
 void Driver::InitTrack(tTrack* p_track, void* p_carHandle, void** p_carParmHandle, tSituation* p_situation) {
     m_humanDriver.init_track(m_index, p_track, p_carHandle, p_carParmHandle, p_situation);
 
-    Mediator::GetInstance()->RaceStart(p_track, p_carHandle, p_carParmHandle, p_situation);
+//    Mediator::GetInstance()->RaceStart(p_track, p_carHandle, p_carParmHandle, p_situation);
 }
 
 void Driver::NewRace(tCarElt* p_car, tSituation* p_situation) {
@@ -17,7 +17,7 @@ void Driver::NewRace(tCarElt* p_car, tSituation* p_situation) {
 void Driver::Drive(tCarElt* p_car, tSituation* p_situation) {
     m_humanDriver.drive_at(m_index, p_car, p_situation);
 
-    Mediator::GetInstance()->DriveTick(p_car, p_situation);
+//    Mediator::GetInstance()->DriveTick(p_car, p_situation);
 }
 
 void Driver::PauseRace(tCarElt* p_car, tSituation* p_situation) {
@@ -35,7 +35,7 @@ int Driver::PitCmd(tCarElt* p_car, tSituation* p_situation) {
 void Driver::EndRace(tCarElt* p_car, tSituation* p_situation) {
     m_humanDriver.end_race(m_index, p_car, p_situation);
 
-    Mediator::GetInstance()->RaceStop(p_car, p_situation);
+//    Mediator::GetInstance()->RaceStop(p_car, p_situation);
 }
 
 void Driver::Shutdown() {
