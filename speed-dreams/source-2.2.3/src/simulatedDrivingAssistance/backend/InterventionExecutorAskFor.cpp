b106643e5873c2de492d3673ed6a461e2e7c8e93
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-03-15 14:36:39 +0100

    Added cpp files

---
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/InterventionExecutorAskFor.cpp
@@ -0,0 +1,6 @@
+#include "InterventionExecutorAskFor.h"
+
+void InterventionExecutorAskFor::RunDecision(IDecision** p_decisions, int p_decisionCount)
+{
+	std::cout << "Make commands that ask to be done" << std::endl;
+}
--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/InterventionExecutorPerformWhenNeeded.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/InterventionExecutorPerformWhenNeeded.h
@@ -5,8 +5,5 @@
 
 class InterventionExecutorPerformWhenNeeded : public InterventionExecutor
 {
-    void RunDecision(IDecision** p_decisions, int p_decisionCount) override
-    {
-        std::cout << "Make commands that perform when needed" << std::endl;
-    }
+    void RunDecision(IDecision** p_decisions, int p_decisionCount) override;
 };
