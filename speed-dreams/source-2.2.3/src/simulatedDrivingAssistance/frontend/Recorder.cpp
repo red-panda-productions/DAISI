d2f8993d572c934cdb0a455d4658a203963f24fe
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-03-16 13:44:27 +0100

    syntax highlighting fixed

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/Recorder.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/Recorder.cpp
@@ -1,20 +1,20 @@
-//#include "Recorder.h"
-//
-//Recorder::Recorder()
-//{
-//	time_t now = time(0);
-//	char* nowString = ctime(&now);
-//	m_FileName = "Data/Recordings/" + nowString + ".txt";
-//	m_RecordingFile = new std::ofstream(m_FileName, std::ios::app);
-//	m_RecordingFile.close()
-//
-//}
-//
-//
-//void Recorder::WriteRecording(float* p_Input)
-//{
-//	m_RecordingFile.open(m_FileName);
-//
-//	m_RecordingFile.close(m_FileName);
-//
-//}
+#include "Recorder.h"
+
+Recorder::Recorder()
+{
+	time_t now = time(0);
+	char* nowString = ctime(&now);
+	m_FileName = "Data/Recordings/" + nowString + ".txt";
+	m_RecordingFile = new std::ofstream(m_FileName, std::ios::app);
+	m_RecordingFile.close()
+
+}
+
+
+void Recorder::WriteRecording(float* p_Input)
+{
+	m_RecordingFile.open(m_FileName);
+
+	m_RecordingFile.close(m_FileName);
+
+}
--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/Recorder.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/Recorder.h
@@ -1,16 +1,16 @@
-//#pragma once
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <ctime>
-//#include <utility>
-//class Recorder
-//{
-//public:
-//	Recorder();
-//	void WriteRecording(float* p_input);
-//
-//private:
-//	std::ofstream m_RecordingFile;
-//	std::string m_FileName;
-//};
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <utility>
+class Recorder
+{
+public:
+	Recorder();
+	void WriteRecording(float* p_input);
+
+private:
+	std::ofstream m_RecordingFile;
+	std::string m_FileName;
+};
