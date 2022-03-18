6e4898389b62b4c119b98813aaca433cd5fe0b51
Sindarin27 <sindarin27@gmail.com>
2022-03-18 13:24:18 +0100

    Finish basic test, add #define's for commonly used expressions, add timestamp argument

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/FileDataStorageTests.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/FileDataStorageTests.cpp
@@ -2,18 +2,22 @@
 #include "TestUtils.h"
 #include "FileDataStorage.h"
 
-TEST(FileStorageTest, BasicFile)
+TEST(FileStorageTest, NoStorageTimestampZero)
 {
-    bool params[2] = {true, true};
+    // Initialise class
+    bool params[5] = {false, false, false, false, false};
     FileDataStorage fileDataStorage = FileDataStorage(params);
 
+    // Write a file with user id, save timestamp 0, and shut down
     fileDataStorage.Initialise("testDataStorage.txt", "player1");
-    fileDataStorage.Save(nullptr);
+    fileDataStorage.Save(nullptr, 0);
     fileDataStorage.Shutdown();
 
+    // Read the written file
     std::ifstream reader("testDataStorage.txt");
-    std::stringstream buffer;
-    buffer << reader.rdbuf();
-    ASSERT_EQ(buffer.str(), "USER:player1\nSTART:0\nEND:0\n");
+    std::string fileContents((std::istreambuf_iterator<char>(reader)), std::istreambuf_iterator<char>());
+    reader.close();
 
+    // Check contents
+    ASSERT_EQ(fileContents, "player1\n0\n");
 }
