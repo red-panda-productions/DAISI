2841b798db02f57c880e4a812bda75c47b719d3a
shoekzema <57985160+shoekzema@users.noreply.github.com>
2022-03-18 15:46:01 +0100

    Added set-functions in Mediator and refactor InterventionType tests
    Now split in MediatorTests and FactoryTests

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/Mediator.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/Mediator.h
@@ -20,8 +20,25 @@ public:
 	void RaceStart(tTrack* p_track, void* p_carHandle, void** p_carParmHandle, tSituation* p_situation);
 	void RaceStop();
 
-    // calls decision maker to change its intervention setting to p_type
-	void SetInterventionType(InterventionType p_type);
+    /// @brief        Sets the task in SDAConfig to p_task
+    /// @param p_task The Task
+	void SetTask(Task p_task);
+
+    /// @brief              Sets the settings for indication of interventions
+    /// @param p_indicators The Indicator settings
+    void SetIndicatorSettings(bool* p_indicators);
+
+    /// @brief        Sets the settings for interventionType to p_type
+    /// @param p_type The InterventionType
+    void SetInterventionType(InterventionType p_type);
+
+    /// @brief           Sets the maximum simulationTime to p_maxTime
+    /// @param p_maxTime The userID
+    void SetMaxTime(int p_maxTime);
+
+    /// @brief          Sets the userID to p_userID
+    /// @param p_userID The userID
+    void SetUserID(char* p_userID);
 
     /// @brief               Sets the settings for data collection
     /// @param p_dataSetting An array of booleans to enable/disable the collection of simulation data for research
