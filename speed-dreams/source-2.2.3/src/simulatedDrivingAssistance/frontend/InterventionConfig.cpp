fd9c16b2c3e0dab82d31a0924cee47fe2bbb7ff4
Kwant,L.C. de (Lars) <l.c.dekwant@students.uu.nl>
2022-03-20 17:17:23 +0100

    implemented loading intervention texture data from XML

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/InterventionConfig.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/InterventionConfig.cpp
@@ -1,1 +1,55 @@
+#include <fstream>
+#include <string>
+#include <portability.h>
+
 #include "InterventionConfig.h"
+
+void InterventionConfig::SetInterventionAction(InterventionAction p_action) 
+{
+    m_currentAction = p_action;
+}
+
+void InterventionConfig::SetTextures(tTextureData* p_textures) 
+{
+    m_textures = p_textures;
+}
+
+tTextureData InterventionConfig::GetCurrentInterventionTexture() 
+{
+    return m_textures[m_currentAction];
+}
+
+InterventionConfig* InterventionConfig::GetInstance() 
+{
+    if (m_instance == nullptr)
+    {
+        // check if InterventionConfig file exists
+        struct stat info;
+        char workingDir[256];
+        getcwd(workingDir, 256);
+        std::string workingDirectory(workingDir);
+        workingDirectory += "\\Singletons\\InterventionConfig";
+        const char* filepath = workingDirectory.c_str();
+        int err = stat(filepath, &info);
+        if (err == -1)
+        {
+            // file does not exist create pointer
+            m_instance = new InterventionConfig();
+            std::ofstream file("Singletons/InterventionConfig");
+            file << m_instance;
+            file.close();
+            return m_instance;
+        }
+
+        // file exists read pointer
+        std::string pointerName("00000000");
+        std::ifstream file("Singletons/InterventionConfig");
+        getline(file, pointerName);
+        file.close();
+        int pointerValue = stoi(pointerName, 0, 16);
+        m_instance = (InterventionConfig*)pointerValue;
+    }
+    return m_instance;
+}
+
+InterventionConfig* InterventionConfig::m_instance = nullptr;
--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/InterventionConfig.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/frontend/InterventionConfig.h
@@ -3,6 +3,67 @@
 #include <plib/ssg.h>
 #include "ConfigEnums.h"
 
-class InterventionConfig {
+// Location of the intervention XML file, with respect to the root data directory.
+#define INTERVENTION_DATA_DIR_FORMAT "%sdata/intervention/intervention.xml"
 
+// Parameters of sections and attributes used in the XML file.
+#define PRM_SECT_INTERVENTIONS  "Interventions"
+#define PRM_ATTR_NAME           "name"
+#define PRM_ATTR_TEXTURE        "texture"
+#define PRM_ATTR_XPOS           "xpos"
+#define PRM_ATTR_YPOS           "ypos"
+
+
+typedef struct ScreenPosition 
+{
+    int X, Y;
+} tScreenPosition;
+
+
+typedef struct TextureData 
+{
+    const char* Name;
+    ssgSimpleState* Texture;
+    tScreenPosition Position;
+
+    TextureData() = default;
+    TextureData(const char* name, ssgSimpleState* tex, tScreenPosition pos)
+        : Name(name) , Texture(tex) , Position(pos) { }
+} tTextureData;
+
+
+class InterventionConfig 
+{
+public:
+
+    /// @brief          Sets the current active intervention action
+    /// @param p_action The action to set as current
+    void SetInterventionAction(InterventionAction p_action);
+
+    /// @brief              Sets the textures that are used by the HUD
+    /// @param p_textures   An array containing the texture data, 
+    ///                     indexed by the InterventionAction type in ConfigEnums.h
+    void SetTextures(tTextureData* p_textures);
+
+    /// @brief  Retrieves the texture belonging to the current intervention action
+    /// @return The texture data
+    tTextureData GetCurrentInterventionTexture();
+
+
+    /// @brief  Returns an the existing InterventionConfig, or creates the initial one if it doesn't exist yet.
+    /// @return The InterventionConfig instance
+    static InterventionConfig* GetInstance();
+
+    /// @brief Removes copy constructor for singleton behaviour
+    InterventionConfig(InterventionConfig const&) = delete;
+
+    /// @brief Removes assigment for singleton behaviour
+    void operator=(InterventionConfig const&) = delete;
+
+private:
+    InterventionConfig() = default;
+    static InterventionConfig* m_instance;
+
+    InterventionAction m_currentAction;
+    tTextureData* m_textures;
 };
