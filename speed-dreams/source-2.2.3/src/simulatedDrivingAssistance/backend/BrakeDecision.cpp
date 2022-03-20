dea2ed026a8c795b75f45df069b606df8b69ebd5
Kwant,L.C. de (Lars) <l.c.dekwant@students.uu.nl>
2022-03-20 15:20:51 +0100

    move and update Enum type for intervention actions

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/BrakeDecision.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/BrakeDecision.cpp
@@ -1,10 +1,11 @@
 #include "BrakeDecision.h"
 #include "Mediator.h"
+
 void BrakeDecision::RunIndicateCommands()
 {
 	if (m_brakeAmount < 0.5f) return;
 
-	SMediator::GetInstance()->CarController.ShowUI(UI_TYPE_BRAKE);
+	SMediator::GetInstance()->CarController.ShowUI(INTERVENTION_ACTION_BRAKE);
 }
 
 void BrakeDecision::RunInterveneCommands()
