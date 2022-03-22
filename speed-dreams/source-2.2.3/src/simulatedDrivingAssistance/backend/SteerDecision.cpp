2afbc6d95e45760f5ff5442472daee05723d1b4c
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-03-22 10:50:48 +0100

    Steering threshold rename

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SteerDecision.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SteerDecision.cpp
@@ -1,23 +1,23 @@
 #include "SteerDecision.h"
 #include "Mediator.h"
 
-#define STEERING_THRESHOLD 0.04f
+#define SDA_STEERING_THRESHOLD 0.04f
 
 void SteerDecision::RunIndicateCommands()
 {
-	if(m_steerAmount < -STEERING_THRESHOLD)
+	if(m_steerAmount < -SDA_STEERING_THRESHOLD)
 	{
 		SMediator::GetInstance()->CarController.ShowUI(INTERVENTION_ACTION_TURN_RIGHT);
 		return;
 	}
-	if(m_steerAmount > STEERING_THRESHOLD)
+	if(m_steerAmount > SDA_STEERING_THRESHOLD)
 	{
 		SMediator::GetInstance()->CarController.ShowUI(INTERVENTION_ACTION_TURN_LEFT);
 	}
 };
 void SteerDecision::RunInterveneCommands()
 {
-	if (m_steerAmount > -STEERING_THRESHOLD && m_steerAmount < STEERING_THRESHOLD) return;
+	if (m_steerAmount > -SDA_STEERING_THRESHOLD && m_steerAmount < SDA_STEERING_THRESHOLD) return;
 
 	SMediator::GetInstance()->CarController.SetSteerCmd(m_steerAmount);
 };
