6aff7bd75beb8bb8ec6d058965de49452a41394e
Sindarin27 <sindarin27@gmail.com>
2022-04-05 17:19:02 +0200

    Use Debug version of Connector C++, finish database connection
    - Replace Release version of Connector C++ with debug version to avoid errors when running on debug profile
    - Replace port string in OpenDatabase with port integer
    - Use connection options in OpenDatabase instead of argument connect. This allows using more arguments, such as reconnect and multi-statements
    - Create a database schema if it does not yet exist
    - Use latest version of SQL create statements
    - Actually use password property in test

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.h
@@ -13,7 +13,7 @@ public:
     SQLDatabaseStorage();
     void StoreData(const std::string p_inputFilePath) override;
     void OpenDatabase(const std::string& p_hostName,
-                      const std::string& p_port,
+                      int p_port,
                       const std::string& p_username,
                       const std::string& p_password,
                       const std::string& p_schemaName,
