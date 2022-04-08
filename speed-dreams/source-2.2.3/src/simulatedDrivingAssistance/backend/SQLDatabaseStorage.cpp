41340df21fdcce35a961d15d28f1dbcc9d6a4f22
Sindarin27 <sindarin27@gmail.com>
2022-04-08 17:46:44 +0200

    Enforce SSL connection when connecting to database
    - Set sslEnforce property to true when connecting to database, such that a database connection is always secure.

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.cpp
@@ -89,6 +89,8 @@ bool SQLDatabaseStorage::OpenDatabase(
     connection_properties["port"] = p_port;
     connection_properties["OPT_RECONNECT"] = true;
     connection_properties["CLIENT_MULTI_STATEMENTS"] = false;
+    connection_properties["sslEnforce"] = true;
+    
     try { m_connection = m_driver->connect(connection_properties);}
     catch (std::exception& e) {return false;}
 
