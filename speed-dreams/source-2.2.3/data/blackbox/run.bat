1df1bb763da699d19386a3f82d42c2e2df3228cd
Sindarin27 <sindarin27@gmail.com>
2022-04-09 18:20:22 +0200

    Include the SDABrakeSample exe instead of empty bat file
    - Make the run.bat blackbox start SDABrakeSample.exe for the black box with an additional message
    - Include the latest build of SDABrakeSample and IPCLib.dll in the data/blackbox folder

--- speed-dreams/source-2.2.3/data/blackbox/run.bat
+++ speed-dreams/source-2.2.3/data/blackbox/run.bat
@@ -1,2 +1,3 @@
 @echo off
-echo I am not actually a black box, I am just pretending to be one because a selection menu still has to be added! For now, please start a real black box yourself :)
+echo Starting the SDABrakeSample black box!
+start SDABrakeSample.exe
