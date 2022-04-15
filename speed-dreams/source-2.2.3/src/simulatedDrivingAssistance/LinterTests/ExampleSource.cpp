2a3f75d3e25495f3b2d6d9a85b84968da42e5111
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-04-15 17:51:58 +0200

    conformed source to clang

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/LinterTests/ExampleSource.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/LinterTests/ExampleSource.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
 #include "ExampleHeader.hpp"
 
-
-void ExampleClass::Hello(int p_name) { std::cout << p_name << std::endl;}
+void ExampleClass::Hello(int p_name)
+{
+    std::cout << p_name << std::endl;
+}
