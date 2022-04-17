5f456505089ca4919e11c6625db1e9ba99381fba
Sindarin27 <sindarin27@gmail.com>
2022-04-17 09:28:19 +0200

    Rename Initialise to Initialize one last time

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/FileDataStorage.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/FileDataStorage.h
@@ -10,7 +10,7 @@ class FileDataStorage {
  private:
     /// @brief Boolean array to determine what to save and what not to save. Uses indices as in ConfigEnums.h
     tDataToStore m_saveSettings;
-    /// @brief Output filestream to write data to, should be initialized through @link FileDataStorage::Initialise
+    /// @brief Output filestream to write data to, should be initialized through @link FileDataStorage::Initialize
     std::ofstream m_outputStream;
  public:
     explicit FileDataStorage(tDataToStore p_saveSettings);
