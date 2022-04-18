5343d2094d025f2acf8f07571f697a4a1355256a
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-04-18 14:00:16 +0200

    removed example header and source from files

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/LinterTests/ExampleHeader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class ExampleClass
-{
-public:
-    void Hello(int p_name);
-};
--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/LinterTests/ExampleSource.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "ExampleHeader.h"
-#include "Mediator.h"
-
-void ExampleClass::Hello(int p_name)
-{
-    std::cout << p_name << std::endl;
-}
