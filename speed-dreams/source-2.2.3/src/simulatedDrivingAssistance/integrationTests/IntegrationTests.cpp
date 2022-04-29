26b7a82d267a5e68c17facc321ccdd24fb1fc7b2
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-04-29 09:45:07 +0200

    removed binary file test

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/integrationTests/IntegrationTests.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/integrationTests/IntegrationTests.cpp
@@ -9,15 +9,19 @@
 
 #define SD_EXECUTABLE "speed-dreams-2.exe"
 
-#define CAR_FILE "car.xml"
+#define CAR_FILE       "car.xml"
 #define DECISIONS_FILE "decisions.txt"
 #define RECORDING_FILE "recording.txt"
 
-#define CAR_ARG "--carxml="
+#define CAR_ARG       "--carxml="
 #define DECISIONS_ARG "--decisions="
 #define RECORDING_ARG "--recording="
 
-class IntegrationTests : public testing::TestWithParam<std::string> {};
+#define BB_FILE_ARG "--bbfile="
+
+class IntegrationTests : public testing::TestWithParam<std::string>
+{
+};
 
 void RunTest(const std::string& p_path)
 {
@@ -25,7 +29,7 @@ void RunTest(const std::string& p_path)
     std::experimental::filesystem::path decisions;
     std::experimental::filesystem::path recording;
 
-    for (const auto &entry : std::experimental::filesystem::directory_iterator(p_path))
+    for (const auto& entry : std::experimental::filesystem::directory_iterator(p_path))
     {
         std::experimental::filesystem::path file = entry.path();
         std::experimental::filesystem::path filename = file.filename();
@@ -60,7 +64,7 @@ void RunTest(const std::string& p_path)
     std::string argsstring = args.str();
 
     PROCESS_INFORMATION processInformation;
-    StartProcess(SD_EXECUTABLE, argsstring.c_str(),processInformation);
+    StartProcess(SD_EXECUTABLE, argsstring.c_str(), processInformation);
 
     WaitForSingleObject(processInformation.hProcess, INFINITE);
 
@@ -76,37 +80,6 @@ void RunTest(const std::string& p_path)
 
 TEST_P(IntegrationTests, IntegrationTest)
 {
-    float value1 = 0;
-    float value2 = 1;
-    float value3 = 4.5;
-    int value4 = 10;
-
-    std::ofstream ofile("hi",  std::ios::binary);
-
-    if (ofile.bad()) throw std::exception();
-
-    ofile.write(reinterpret_cast<char*>(&value1),sizeof(float));
-    ofile.write(reinterpret_cast<char*>(&value2),sizeof(float));
-    ofile.write(reinterpret_cast<char*>(&value3),sizeof(float));
-    ofile.write(reinterpret_cast<char*>(&value4),sizeof(int));
-    ofile.close();
-
-    float checkvalue1 = -1;
-    float checkvalue2 = -1;
-    float checkvalue3 = -1;
-    int checkvalue4 = -1;
-
-    if (ofile.bad()) throw std::exception();
-
-    std::ifstream ifile("hi", std::ios::binary);
-    
-    ifile.read(reinterpret_cast<char*>(&checkvalue1), sizeof(float));
-    ifile.read(reinterpret_cast<char*>(&checkvalue2), sizeof(float));
-    ifile.read(reinterpret_cast<char*>(&checkvalue3), sizeof(float));
-    ifile.read(reinterpret_cast<char*>(&checkvalue4), sizeof(float));
-
-    ifile.close();
-
     RunTest(GetParam());
 }
 
