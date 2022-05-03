dd785d3019587d93d28e60841049fc1dc44609bf
VoidLeech <ian.gerritse@gmail.com>
2022-05-03 16:45:28 +0200

    Clarify use of INVERT_COMP_TEST

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/unitTestUtilities/ComparerUtils.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/unitTestUtilities/ComparerUtils.h
@@ -3,11 +3,14 @@
 #include "raceman.h"
 #include <gtest/gtest-spi.h>
 
-/// @brief Inverts a test. Since these comparisons have many tests, a couple naive methods of inverting don't work: \
-/// introducing a variable that switchers every EQ with NE and vice versa; would require inequality of all values, \
-/// EXPECT_(NON)FATAL_FAILURE; expects a single failure, \
+/// @brief Inverts a test. ONLY INTENDED FOR calling with functions provided in this header file, and only with p_eqOrNe equal to true. \n
+/// Since these comparisons have many tests, a couple naive methods of inverting don't work: \n
+/// introducing a variable that switchers every EQ with NE and vice versa; would require inequality of all values, \n
+/// EXPECT_(NON)FATAL_FAILURE; expects a single failure, \n
 /// chaining EXPECT_NONFATAL_FAILURE with the above; fails if there is just one failure
 /// @note Scoped so the reporter is not useable outside of this inversion
+/// @note Strictly speaking will not invert a test with p_eqOrNe fully, as having a single pointer equal would already fail the inner test
+/// (and thus succeed), but calling INVERT_COMP_TEST with an inner call with p_eqOrNe == false is not the intended use case
 #define INVERT_COMP_TEST(a)                                                                                                                        \
     {                                                                                                                                              \
         testing::TestPartResultArray failures;                                                                                                     \
