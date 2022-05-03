407f42c366d1c655f687b25bcc235838dd8d3160
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-05-03 09:15:01 +0200

    added comments for tests

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/CarControllerTests.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/CarControllerTests.cpp
@@ -5,11 +5,13 @@
 #include "IndicatorConfig.h"
 #include "TestUtils.h"
 
+/// @brief sets up teh car controller
 #define SETUP_CAR                \
     tCarElt car = {};            \
     CarController carController; \
     carController.SetCar(&car)
 
+/// @brief tests a controller param given a function
 #define TEST_CAR_CONTROLLER_PARAM(p_paramFunction)               \
     SETUP_CAR;                                                   \
     Random random;                                               \
@@ -18,6 +20,9 @@
         p_paramFunction(carController, random.NextFloat(-1, 1)); \
     }
 
+/// @brief                 Tests the steer parameter of a car controller
+/// @param p_carController The car controller
+/// @param p_steer         The steer value
 void SteerTest(CarController p_carController, float p_steer)
 {
     float start = p_carController.GetCar()->ctrl.steer;
@@ -25,6 +30,9 @@ void SteerTest(CarController p_carController, float p_steer)
     ASSERT_EQ(p_carController.GetSteerCmd(), p_steer + start);
 }
 
+/// @brief                 Tests the acceleration parameter of a car controller
+/// @param p_carController The car controller
+/// @param p_steer         The acceleration value
 void AccelTest(CarController p_carController, float p_accel)
 {
     float start = p_carController.GetCar()->ctrl.accelCmd;
@@ -32,6 +40,9 @@ void AccelTest(CarController p_carController, float p_accel)
     ASSERT_EQ(p_carController.GetAccelCmd(), p_accel + start);
 }
 
+/// @brief                 Tests the brake parameter of a car controller
+/// @param p_carController The car controller
+/// @param p_steer         The brake value
 void BrakeTest(CarController p_carController, float p_brake)
 {
     float start = p_carController.GetCar()->ctrl.brakeCmd;
@@ -39,18 +50,25 @@ void BrakeTest(CarController p_carController, float p_brake)
     ASSERT_EQ(p_carController.GetBrakeCmd(), std::max(p_brake, start));
 }
 
+/// @brief                 Tests the clutch parameter of a car controller
+/// @param p_carController The car controller
+/// @param p_steer         The clutch value
 void ClutchTest(CarController p_carController, float p_clutch)
 {
     p_carController.SetClutchCmd(p_clutch);
     ASSERT_EQ(p_carController.GetClutchCmd(), p_clutch);
 }
 
+/// @brief                 Tests the lights parameter of a car controller
+/// @param p_carController The car controller
+/// @param p_steer         The lights value
 void LightTest(CarController p_carController, bool p_lights)
 {
     p_carController.SetLightCmd(p_lights);
     ASSERT_EQ(p_carController.GetLightCmd(), p_lights);
 }
 
+/// @brief Tests the set car function of car controller
 TEST(CarControllerTests, SetCarTest)
 {
     SETUP_CAR;
@@ -59,26 +77,31 @@ TEST(CarControllerTests, SetCarTest)
     ASSERT_TRUE(pointer == &car);
 }
 
+/// @brief Tests the steer parameter of car controller
 TEST(CarControllerTests, SteerTests)
 {
     TEST_CAR_CONTROLLER_PARAM(SteerTest);
 }
 
+/// @brief Tests the acceleration parameter of car controller
 TEST(CarControllerTests, AccelTests)
 {
     TEST_CAR_CONTROLLER_PARAM(AccelTest);
 }
 
+/// @brief Tests the brake parameter of car controller
 TEST(CarControllerTests, BrakeTests)
 {
     TEST_CAR_CONTROLLER_PARAM(BrakeTest);
 }
 
+/// @brief Tests the clutch parameter of car controller
 TEST(CarControllerTests, ClutchTests)
 {
     TEST_CAR_CONTROLLER_PARAM(ClutchTest);
 }
 
+/// @brief Tests the lights parameter of car controller
 TEST(CarControllerTests, LightTests)
 {
     SETUP_CAR;
@@ -89,6 +112,8 @@ TEST(CarControllerTests, LightTests)
     }
 }
 
+/// @brief          Tests the show intervention function of car controller
+/// @param p_action The intervention action that will be set
 void ShowInterventionTest(InterventionAction p_action)
 {
     IndicatorConfig::ClearInstance();
