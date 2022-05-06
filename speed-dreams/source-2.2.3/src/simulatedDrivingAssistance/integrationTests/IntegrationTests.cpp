a2e55b0dfdca7efba023d8fc1d802001d9a1dd62
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-05-06 15:06:25 +0200

    made changes according to @suppergerrie2 cli arguments

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/integrationTests/IntegrationTests.cpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/integrationTests/IntegrationTests.cpp
@@ -10,56 +10,26 @@
 namespace filesystem = std::experimental::filesystem;
 
 #define SD_EXECUTABLE "speed-dreams-2.exe"
-
-#define CAR_FILE       "car.xml"
-#define DECISIONS_FILE "decisions.txt"
-#define RECORDING_FILE "recording.txt"
-#define BB_FILE        "bbfile.txt"
-
-#define CAR_ARG       "--carxml "
-#define DECISIONS_ARG "--decisions "
-#define RECORDING_ARG "--recording "
-
-#define BB_ARG "--bbfile "
+#define BB_FILE       "decisions.txt"
+#define REPLAY_ARG    "--replay "
+#define SD_EXTRA_ARGS "--textonly"
+#define BB_ARG        "--bbfile "
 
 /// @brief              Checks if all files for an integration test are present in the folder
 ///                     and returns the path to all files if they are present
-/// @param  p_path      The path to the folder
-/// @param  p_carxml    The path to the car.xml file
-/// @param  p_decisions The path to the decisions file
-/// @param  p_recording The path to the recording file
 /// @param  p_bbfile    The path to the black box recording file
-void CheckFiles(const std::string& p_path, filesystem::path& p_carxml, filesystem::path& p_decisions, filesystem::path& p_recording, filesystem::path& p_bbfile)
+void CheckFiles(const filesystem::path& p_path, filesystem::path& p_bbfile)
 {
     for (const auto& entry : std::experimental::filesystem::directory_iterator(p_path))
     {
         filesystem::path file = entry.path();
         filesystem::path filename = file.filename();
         std::string name = filename.string();
-        if (name == CAR_FILE)
-        {
-            p_carxml = file;
-            continue;
-        }
-        if (name == DECISIONS_FILE)
-        {
-            p_decisions = file;
-            continue;
-        }
-        if (name == RECORDING_FILE)
-        {
-            p_recording = file;
-            continue;
-        }
         if (name == BB_FILE)
         {
             p_bbfile = file;
         }
     }
-
-    ASSERT_EQ(p_carxml.filename(), CAR_FILE);
-    ASSERT_EQ(p_decisions.filename(), DECISIONS_FILE);
-    ASSERT_EQ(p_recording.filename(), RECORDING_FILE);
     ASSERT_EQ(p_bbfile.filename(), BB_FILE);
 }
 
@@ -68,14 +38,10 @@ void CheckFiles(const std::string& p_path, filesystem::path& p_carxml, filesyste
 /// @param  p_decisions  The path to the decisions file
 /// @param  p_recording  The path to the recording file
 /// @return              The arguments
-std::string GenerateSimulationArguments(const filesystem::path& p_carxml, const filesystem::path& p_decisions, const filesystem::path& p_recording)
+std::string GenerateSimulationArguments(const filesystem::path& p_path)
 {
     std::stringstream args;
-    args << CAR_ARG << p_carxml;
-    args << " ";
-    args << DECISIONS_ARG << p_decisions;
-    args << " ";
-    args << RECORDING_ARG << p_recording;
+    args << REPLAY_ARG << p_path;
 
     return {args.str()};
 }
@@ -87,7 +53,7 @@ std::string GenerateBBArguments(const filesystem::path& p_bbfile)
 {
     std::stringstream args;
 
-    args << BB_ARG << p_bbfile;
+    args << BB_ARG << p_bbfile << " " << SD_EXTRA_ARGS;
 
     return {args.str()};
 }
@@ -114,14 +80,11 @@ void CheckProcess(PROCESS_INFORMATION p_processInformation)
 /// @param  p_path The path to the integration test folder
 void RunTest(const std::string& p_path)
 {
-    filesystem::path carxml;
-    filesystem::path decisions;
-    filesystem::path recording;
     filesystem::path bbfile;
 
-    CheckFiles(p_path, carxml, decisions, recording, bbfile);
+    CheckFiles(p_path, bbfile);
 
-    std::string simulationArgs = GenerateSimulationArguments(carxml, decisions, recording);
+    std::string simulationArgs = GenerateSimulationArguments(p_path);
 
     PROCESS_INFORMATION simulationInfo;
     StartProcess(SD_EXECUTABLE, simulationArgs.c_str(), simulationInfo);
