0a83db13b1cdb90072d30294f908aa8d86af1233
Meaganium <44114583+Meaganium@users.noreply.github.com>
2022-05-12 17:44:31 +0200

    EndExperiment.cpp conforms to CLang standards

--- speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/EndExperiment.cpp
+++ speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/EndExperiment.cpp
@@ -11,7 +11,7 @@ static void* MenuHandle = NULL;
 static void
 onAcceptExit(void* /* dummy */)
 {
-    //SMediator::GetInstance()->SetSaveRaceToDatabase(false);
+    // SMediator::GetInstance()->SetSaveRaceToDatabase(false);
     LmRaceEngine().abortRace();  // Do cleanup to get back correct setup files
     LegacyMenu::self().quit();
 }
@@ -27,38 +27,32 @@ void* EndExperimentInit(void* p_menuHandle, RaceEndType p_saveWayVersion)
 
     void* param = GfuiMenuLoad("endexperimentscreen.xml");
     GfuiMenuCreateStaticControls(MenuHandle, param);
-    //GfuiMenuCreateStaticControls(MenuHandle, param);
-
+    // GfuiMenuCreateStaticControls(MenuHandle, param);
 
     switch (p_saveWayVersion)
     {
-    case NO_END:
-    {
-        GfuiMenuCreateButtonControl(MenuHandle, param, "ok", NULL, onAcceptExit);
+        case NO_END:
+        {
+            GfuiMenuCreateButtonControl(MenuHandle, param, "ok", NULL, onAcceptExit);
 
-        break;
+            break;
+        }
+        case RESTART:
+        {
+        }
+        case EXIT:
+        {
+        }
+        case ABORT:
+        {
+        }
+        default:
+        {
+            throw std::runtime_error("incorrect 'p_experimentWayVersion', have you defined the new option in endexperiment.h?");
+        }
     }
-    case RESTART:
-    {
-    
-    }
-    case EXIT: 
-    {
-    
-    }
-    case ABORT:
-    {
-    
-    }
-    default:
-    {
-        throw std::runtime_error("incorrect 'p_experimentWayVersion', have you defined the new option in endexperiment.h?");
-    }
-
-    }
-    //GfuiMenuCreateButtonControl(MenuHandle, param, "ok", NULL, onAcceptExit);
+    // GfuiMenuCreateButtonControl(MenuHandle, param, "ok", NULL, onAcceptExit);
     GfuiAddKey(MenuHandle, GFUIK_SPACE, "No, back to the game", NULL, onAcceptExit, NULL);
-
 
     GfParmReleaseHandle(param);
 
