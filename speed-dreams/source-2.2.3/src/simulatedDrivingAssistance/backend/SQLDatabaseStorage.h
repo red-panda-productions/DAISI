9b66160964bfb7304c9c509dae40264d2266e6ee
Meaganium <44114583+Meaganium@users.noreply.github.com>
2022-05-31 15:30:55 +0200

    Added more booleans and return false where necessary

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backend/SQLDatabaseStorage.h
@@ -11,7 +11,7 @@ class SQLDatabaseStorage : IDataStorage
 {
 public:
     SQLDatabaseStorage();
-    void Run(const std::experimental::filesystem::path& p_inputFilePath, const std::string& p_dirPath = "");
+    bool Run(const std::experimental::filesystem::path& p_inputFilePath, const std::string& p_dirPath = "");
     bool StoreData(const std::experimental::filesystem::path& p_inputFilePath) override;
     bool OpenDatabase(DatabaseSettings p_dbSettings,
                       const std::string& p_dirPath = "");
