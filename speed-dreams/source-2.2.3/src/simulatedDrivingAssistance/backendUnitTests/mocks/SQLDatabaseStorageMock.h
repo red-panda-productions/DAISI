3a80f937e2e9d207ae627ce7a94173d1bf29a9f0
VoidLeech <ian.gerritse@gmail.com>
2022-06-02 23:27:25 +0200

    Fix nocoverage SQLDatabaseStorageMock

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/mocks/SQLDatabaseStorageMock.h
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/backendUnitTests/mocks/SQLDatabaseStorageMock.h
@@ -22,11 +22,10 @@ public:
 
         VariableStore::GetInstance().Variables[0] = static_cast<void*>(path);
     }
-    // This function is needed for building (IDataStorage is abstract)
-    // but is never called by DecisionMaker (which is the class where when testing it, we mock SQLDatabaseStorage)
-    bool StoreData(const std::experimental::filesystem::path& p_inputFilePath) override
+    bool StoreData(const std::experimental::filesystem::path& p_inputFilePath) override //@NOCOVERAGE, This function is needed for building
+    // (IDataStorage is abstract) but is never called by DecisionMaker (which is the class where when testing it, we mock SQLDatabaseStorage)
     {
-        return true;  //@NOCOVERAGE, Needed a return type, but as mentioned we don't call the method where we mock this
+        return true;  //@NOCOVERAGE, Needed a return value, but as mentioned we don't call the method where we mock this
     }                 //@NOCOVERAGE, Per above
     bool OpenDatabase(const std::string& p_hostName,
                       int p_port,
