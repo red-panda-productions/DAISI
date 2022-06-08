352829b366c9daf51f2df2685e300731be2bb8c2
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2022-06-08 10:52:49 +0200

    Add more debug output

--- speed-dreams/source-2.2.3/src/modules/simu/simuv4/carstruct.h
+++ speed-dreams/source-2.2.3/src/modules/simu/simuv4/carstruct.h
@@ -143,17 +143,42 @@ typedef struct
 
 #else
 
-#define DUMP_V3(v3, f) \
-    f << #v3 << ": " << v3.x << " " << v3.y << " " << v3.z << " " << std::endl;
+#include <inttypes.h>
+
+#define hexify(x, y, z, f)                   \
+    {                                        \
+        union                                \
+        {                                    \
+            float f;                         \
+            uint32_t u;                      \
+        } f2u;                               \
+        f2u.f = x;                           \
+                                             \
+        f << std::hex << f2u.u << " ";       \
+        f2u.f = y;                           \
+        f << std::hex << f2u.u << " ";       \
+        f2u.f = z;                           \
+        f << std::hex << f2u.u << std::dec << std::endl; \
+    }
+
+#define DUMP_V3(v3, f)                                                          \
+    f << #v3 << ": " << v3.x << " " << v3.y << " " << v3.z << " " << std::endl; \
+    hexify(v3.x, v3.y, v3.z, f);
 
 #define CHECK_VAR(_var_, _msg_)
-#define CHECK(_car_) \
-DUMP_V3(car->carElt->pub.DynGC.pos, debugLogFile) \
-DUMP_V3(car->carElt->pub.DynGC.vel, debugLogFile) \
-DUMP_V3(car->carElt->pub.DynGC.acc, debugLogFile) \
-DUMP_V3(car->carElt->pub.DynGCg.pos, debugLogFile) \
-DUMP_V3(car->carElt->pub.DynGCg.vel, debugLogFile) \
-DUMP_V3(car->carElt->pub.DynGCg.acc, debugLogFile)\
+#define CHECK(_car_)                                   \
+    DUMP_V3(car->carElt->pub.DynGC.pos, debugLogFile)  \
+    DUMP_V3(car->carElt->pub.DynGC.vel, debugLogFile)  \
+    DUMP_V3(car->carElt->pub.DynGC.acc, debugLogFile)  \
+    DUMP_V3(car->carElt->pub.DynGCg.pos, debugLogFile) \
+    DUMP_V3(car->carElt->pub.DynGCg.vel, debugLogFile) \
+    DUMP_V3(car->carElt->pub.DynGCg.acc, debugLogFile) \
+    DUMP_V3(car->DynGC.pos, debugLogFile)  \
+    DUMP_V3(car->DynGC.vel, debugLogFile)  \
+    DUMP_V3(car->DynGC.acc, debugLogFile)  \
+    DUMP_V3(car->DynGCg.pos, debugLogFile) \
+    DUMP_V3(car->DynGCg.vel, debugLogFile) \
+    DUMP_V3(car->DynGCg.acc, debugLogFile) \
     debugLogFile.flush();
 #endif
 
