0a830a6fa383ff21b40c1601fea900e9e933acbd
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-06-08 13:19:41 +0200

    added define for division

--- speed-dreams/source-2.2.3/src/drivers/assistedhuman/Driver.cpp
+++ speed-dreams/source-2.2.3/src/drivers/assistedhuman/Driver.cpp
@@ -45,7 +45,7 @@ void Driver::InitTrack(tTrack* p_track, void* p_carHandle, void** p_carParmHandl
     SMediator::GetInstance()->RaceStart(p_track, p_carHandle, p_carParmHandle, p_situation, m_recorder);
     auto end = std::chrono::system_clock::now();
 
-    long milliseconds = static_cast<long>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 5);
+    long milliseconds = static_cast<long>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / BLACK_BOX_TESTS);
 
     if (milliseconds > TIMEOUT_WARNING)
     {
