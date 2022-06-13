d6ada92a860f7010ab24c403ac06367040d15c02
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-06-13 22:25:25 +0200

    updated changes comment

--- speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/ResearcherMenu.cpp
+++ speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/ResearcherMenu.cpp
@@ -1,6 +1,7 @@
 #include <tgfclient.h>
 #include <random>
 #include <forcefeedback.h>
+#include "tgfclient.h"
 #include "guimenu.h"
 #include "Mediator.h"
 #include "ResearcherMenu.h"
@@ -70,8 +71,8 @@
 #define MSG_ERROR_NO_UID            "You need to have a user id"
 
 // Black box test result messages
-#define MSG_BLACK_BOX_FAST "Fast"
-#define MSG_BLACK_BOX_SLOW "Slow"
+#define MSG_BLACK_BOX_FAST "  Fast"
+#define MSG_BLACK_BOX_SLOW "  Slow"
 #define MSG_BLACK_BOX_TESTING "Testing"
 
 // Black box test result colors
@@ -168,11 +169,14 @@ static void TestBlackBoxAsync()
     // Divided by BLACK_BOX_TESTS to calculate the average response time of the blackbox
     long milliseconds = static_cast<long>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / BLACK_BOX_TESTS);
 
+
+
     if (milliseconds > TIMEOUT_WARNING)
     {
         float slowColor[4] = SLOW_TEXT_COLOR;
         GfuiLabelSetText(s_scrHandle, m_blackBoxTestResultControl, MSG_BLACK_BOX_SLOW);
         GfuiLabelSetColor(s_scrHandle, m_blackBoxTestResultControl, slowColor);
+        
         GfLogWarning("SLOW BLACKBOX: Blackbox took %ld milliseconds (on average) to respond over %i tests, this is relatively slow!\n", milliseconds, BLACK_BOX_TESTS);
         return;
     }
