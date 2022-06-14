8029064bd95d3b6eb970d4a50bbc8e14c798d8eb
shoekzema <57985160+shoekzema@users.noreply.github.com>
2022-06-14 15:09:24 +0200

    Add threshold button to researcher menu
    Also fix the threshold button bug in dev menu

--- speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/DeveloperMenu.h
+++ speed-dreams/source-2.2.3/src/modules/userinterface/legacymenu/mainscreens/DeveloperMenu.h
@@ -3,3 +3,5 @@
 extern void* DeveloperMenuInit(void* p_prevMenu);
 extern void DeveloperMenuRun(void*);
 extern void ConfigureDeveloperSettings();
+extern void RemoteSetDefaultThresholdValues();
+extern void SetTempInterventionType(int p_interventionType);
