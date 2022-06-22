d0182ba9cd00ae6a9e84b2c4d92d05dfd92c22d9
Kwant,L.C. de (Lars) <l.c.dekwant@students.uu.nl>
2022-06-22 14:45:25 +0200

    clang-format FileSystem.hpp

--- speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/rppUtils/FileSystem.hpp
+++ speed-dreams/source-2.2.3/src/simulatedDrivingAssistance/rppUtils/FileSystem.hpp
@@ -8,10 +8,10 @@
 #include <experimental/filesystem>
 
 #ifdef WIN32
-#define OS_SEPARATOR "\\"
+#define OS_SEPARATOR      "\\"
 #define OS_SEPARATOR_CHAR '\\'
 #else
-#define OS_SEPARATOR "/"
+#define OS_SEPARATOR      "/"
 #define OS_SEPARATOR_CHAR '/'
 #endif
 namespace filesystem = std::experimental::filesystem;
